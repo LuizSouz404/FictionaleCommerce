482ba4fe8004f0e7acc8947005c76b5c80c8e536
LuizSouz404 <luiz.souz404@gmail.com>
2021-12-31 22:27:13 -0300

    Dev: header update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,24 +7,18 @@ export function Header() {
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <button><FaUser size={20}/>Login</button>
-
         <a href="/home">
-          <img src="/logo.svg" alt="Up Shooper" />
+          <img className={styles.logo} src="/logo.svg" alt="Up Shooper" />
         </a>
 
-        <button>
-          <FaShoppingBag size={20}/>
-          <div className={styles.active}><span>10</span></div>
-        </button>
+        <div className={styles.containerButtons}>
+          <button className={styles.buttonBadge}>
+            <FaShoppingBag size={16}/>
+            <div className={styles.active}><span>10</span></div>
+          </button>
+          <button className={styles.buttonWithIcon}><FaUser size={16}/><span>Login</span></button>
+        </div>
       </div>
-      <nav className={styles.navbar}>
-        <Link href="estoque/">Todos</Link>
-        <Link href="estoque/camisa">Camisas</Link>
-        <Link href="estoque/casaco">Casacos</Link>
-        <Link href="estoque/calça">Calças</Link>
-        <Link href="estoque/acessorios">Acessórios</Link>
-      </nav>
     </header>
   )
 }
